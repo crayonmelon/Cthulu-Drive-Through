db0f17cdef6eaabe21227b798077f020facb9a03
Ciarán Malone <ciaranmalone27@gmail.com>
2021-02-20 18:22:00 +0000

    pedestrian functionality added, can patrol and be ran over

---
+++ Assets/PedestrianDestination.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PedestrianDestination : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    private GameObject WaypointParent;
+    private Vector3 waypoint;
+    private int randomPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+        WaypointParent = GameObject.FindGameObjectWithTag("WanderWaypointParent");
+        newPoint();
+    }
+
+    private void Update()
+    {
+        if (Vector3.Distance(waypoint, transform.position) < 5f)
+             newPoint();
+    }
+
+    private void newPoint()
+    {
+        randomPoint = Random.Range(0, WaypointParent.transform.childCount);
+        Debug.Log("random number " + randomPoint);
+        waypoint = WaypointParent.transform.GetChild(randomPoint).position;
+        agent.SetDestination(waypoint);
+    }
+}
--- Assets/Scripts/pedestrianAI.cs
+++ Assets/Scripts/pedestrianAI.cs
@@ -1,19 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class OnCollison : MonoBehaviour
+public class pedestrianAI : MonoBehaviour
 {
     [SerializeField] private float forceUp = 5f;
+    
     private Rigidbody rb;
     private LookAtCamera lookAtCamera;
     private Transform childSprite;
+    private PedestrianDestination pedestrianDestination;
+    private NavMeshAgent agent;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         childSprite = this.gameObject.transform.GetChild(0);
         lookAtCamera = childSprite.GetComponent<LookAtCamera>();
-        
+        pedestrianDestination = GetComponent<PedestrianDestination>();
+        agent = GetComponent<NavMeshAgent>();
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -21,13 +27,17 @@ public class OnCollison : MonoBehaviour
         Debug.Log("collision" + collision.transform.tag);
         if (collision.gameObject.CompareTag("Player"))
         {
+            lookAtCamera.enabled = false;
+            pedestrianDestination.enabled = false;
+            agent.enabled = false;
+            childSprite.transform.eulerAngles = new Vector3(0, 0, 0);
+
             rb.constraints = RigidbodyConstraints.None;
+            rb.isKinematic = false;
 
             rb.AddForce(transform.up * forceUp, ForceMode.VelocityChange);
             rb.AddTorque(transform.up * forceUp, ForceMode.VelocityChange);
-            
-            lookAtCamera.enabled = false;
-            childSprite.rotation = Quaternion.Euler(0, 180, 0);
+
         }
     }
 }
